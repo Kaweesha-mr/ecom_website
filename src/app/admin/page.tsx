0f78f58029897459cad1a509c2641e5be9ea8f60
Kaweesha Marasinghe <110246539+Kaweesha-mr@users.noreply.github.com>
2024-07-03 13:22:03 +0530

    feat: cardComponent added

---
+++ src/app/admin/page.tsx
@@ -0,0 +1,39 @@
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+
+export default function AdminDashbord() {
+
+
+    return (
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+
+            <DashbordCard title="f" subtitle="d" body="2" />
+
+
+        </div>
+    )
+}
+
+
+type DashbordCardProps = {
+    title: string
+    subtitle: string
+    body: string
+}
+
+
+ function DashbordCard({ title, subtitle, body }: DashbordCardProps) {
+
+    return <Card>
+        <CardHeader>
+            <CardTitle>{title}</CardTitle>
+            <CardDescription>{subtitle}</CardDescription>
+
+        </CardHeader>
+
+        <CardContent>
+            <p>{body}</p>
+        </CardContent>
+    </Card>
+
+
+}
